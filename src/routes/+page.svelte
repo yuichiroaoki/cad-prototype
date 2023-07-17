f9ba5518cdf465078d9c38c9ca0e63f12fc834b9
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-07-17 21:27:18 +0900

    threejs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,12 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
+	import { Canvas, InteractiveObject, OrbitControls, T } from '@threlte/core';
+	import { spring } from 'svelte/motion';
+	import { degToRad } from 'three/src/math/MathUtils';
+
+	const scale = spring(1);
 </script>
 
 <svelte:head>
@@ -26,6 +32,40 @@
 	</h2>
 
 	<Counter />
+
+	<div>
+		<Canvas>
+			<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
+				<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
+			</T.PerspectiveCamera>
+
+			<T.DirectionalLight castShadow position={[3, 10, 10]} />
+			<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
+			<T.AmbientLight intensity={0.2} />
+
+			<!-- Cube -->
+			<T.Group scale={$scale}>
+				<T.Mesh position.y={0.5} castShadow let:ref>
+					<!-- Add interaction -->
+					<InteractiveObject
+						object={ref}
+						interactive
+						on:pointerenter={() => ($scale = 2)}
+						on:pointerleave={() => ($scale = 1)}
+					/>
+
+					<T.BoxGeometry />
+					<T.MeshStandardMaterial color="#333333" />
+				</T.Mesh>
+			</T.Group>
+
+			<!-- Floor -->
+			<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
+				<T.CircleGeometry args={[3, 72]} />
+				<T.MeshStandardMaterial color="white" />
+			</T.Mesh>
+		</Canvas>
+	</div>
 </section>
 
 <style>
@@ -55,5 +95,10 @@
 		height: 100%;
 		top: 0;
 		display: block;
+	}
+
+	div {
+		height: 100%;
+		width: 100%;
 	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-static'; // This was changed from adapter-auto
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
+import seqPreprocessor from 'svelte-sequential-preprocessor';
+import { preprocessThrelte } from '@threlte/preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	// preprocess: vitePreprocess(),
+	preprocess: seqPreprocessor([preprocess(), preprocessThrelte()]),
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
