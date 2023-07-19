e682a3310e65a23f1e12bcac72b1e12ef28d7966
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-07-19 14:46:31 +0900

    load stl

--- src/routes/GridLine.svelte
+++ src/routes/GridLine.svelte
@@ -2,44 +2,70 @@
 	import { onMount } from 'svelte';
 	import * as THREE from 'three';
 	import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+	import { STLLoader } from 'three/addons/loaders/STLLoader.js';
 
 	let container: HTMLDivElement;
 
 	onMount(() => {
 		// Scene
 		const scene = new THREE.Scene();
+		scene.background = new THREE.Color(0x999999);
 
 		// Camera
-		const camera = new THREE.PerspectiveCamera(75, 1000 / 1000, 0.1, 1000);
-		//   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-		camera.position.z = 5;
+		const camera = new THREE.PerspectiveCamera(35, 1000 / 1000, 1, 500);
+		const cameraTarget = new THREE.Vector3(0, -0.25, 0);
 
-		// Renderer
-		const renderer = new THREE.WebGLRenderer();
-		renderer.setSize(1000, 1000);
-		//   renderer.setSize(window.innerWidth, window.innerHeight);
-		container.appendChild(renderer.domElement);
-		const controls = new OrbitControls(camera, renderer.domElement);
+		camera.position.set(0, 0, 5);
 
 		// Grid
-		const size = 10;
-		const divisions = 10;
-		const gridHelper = new THREE.GridHelper(size, divisions);
+		const size = 50;
+		const divisions = 50;
+		const gridHelper = new THREE.GridHelper(size, divisions, 0xffffff, 0x555555);
+		gridHelper.position.y = -0.5;
+
 		scene.add(gridHelper);
+		gridHelper.receiveShadow = true;
 
-		gridHelper.rotation.x += 0.5;
-		gridHelper.rotation.y += 0.1;
+		// Renderer
+		const renderer = new THREE.WebGLRenderer({ antialias: true });
+		renderer.setSize(1000, 1000);
+		renderer.useLegacyLights = false;
 
-		function animate() {
-			requestAnimationFrame(animate);
+		container.appendChild(renderer.domElement);
 
-			// required if controls.enableDamping or controls.autoRotate are set to true
-			controls.update();
+		const loader = new STLLoader();
+		loader.load('src/lib/images/slotted_disk.stl', function (geometry: any) {
+			const material = new THREE.MeshPhongMaterial({
+				color: 0xff9c7c,
+				specular: 0x494949,
+				shininess: 200
+			});
+			const mesh = new THREE.Mesh(geometry, material);
+			mesh.position.set(0, -0.25, 0.6);
+			mesh.rotation.set(0, -Math.PI / 2, 0);
+			mesh.scale.set(0.5, 0.5, 0.5);
+
+			mesh.castShadow = true;
+			mesh.receiveShadow = true;
+
+			scene.add(mesh);
+
+			render();
+		});
+
+		// Lights
+		scene.add(new THREE.HemisphereLight(0x8d7c7c, 0x494966, 3));
+
+		const controls = new OrbitControls(camera, renderer.domElement);
+		controls.addEventListener('change', render);
+		controls.target.set(0, 0, 2);
+		controls.update();
+
+		function render() {
+			camera.lookAt(cameraTarget);
 
 			renderer.render(scene, camera);
 		}
-
-		animate();
 
 		// Clean up the Three.js scene on component unmount
 		return () => {
