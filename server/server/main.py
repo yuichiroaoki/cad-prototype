4c4a17080026a8dc550c350785fb660f22ea2608
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-07-21 10:46:32 +0900

    load stl from server

--- server/server/main.py
+++ server/server/main.py
@@ -1,9 +1,25 @@
 from pydantic import BaseModel
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from server.path import cq_profile
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class PathConfig(BaseModel):
     diameter: int
@@ -20,3 +36,7 @@ def health_check():
 async def use_profile(conf: PathConfig):
     cq_profile.run_cq_profile(conf.diameter)
     return {"status": "ok"}
+
+@app.get("/load/model/{model_id}")
+async def load_model(model_id: str):
+    return FileResponse(f"data/{model_id}.stl")
--- ui/src/routes/GridLine.svelte
+++ ui/src/routes/GridLine.svelte
@@ -13,9 +13,26 @@
 
 		// Camera
 		const camera = new THREE.PerspectiveCamera(35, 1000 / 1000, 1, 500);
-		const cameraTarget = new THREE.Vector3(0, -0.25, 0);
 
-		camera.position.set(0, 0, 5);
+		camera.position.set(0, 0, 20);
+
+		const loader = new STLLoader();
+		loader.load('http://127.0.0.1:8000/load/model/profile_shape', function (geometry: any) {
+			let material = new THREE.MeshPhongMaterial({
+				color: 0xff9c7c,
+				specular: 0x494949,
+				shininess: 200
+			});
+			// Colored binary STL
+			if (geometry.hasColors) {
+				material = new THREE.MeshPhongMaterial({ opacity: geometry.alpha, vertexColors: true });
+			}
+			const mesh = new THREE.Mesh(geometry, material);
+			mesh.castShadow = true;
+			mesh.receiveShadow = true;
+
+			scene.add(mesh);
+		});
 
 		// Grid
 		const size = 50;
@@ -33,39 +50,19 @@
 
 		container.appendChild(renderer.domElement);
 
-		const loader = new STLLoader();
-		// https://github.com/mrdoob/three.js/blob/master/examples/models/stl/ascii/slotted_disk.stl
-		loader.load('src/lib/images/slotted_disk.stl', function (geometry: any) {
-			const material = new THREE.MeshPhongMaterial({
-				color: 0xff9c7c,
-				specular: 0x494949,
-				shininess: 200
-			});
-			const mesh = new THREE.Mesh(geometry, material);
-			mesh.position.set(0, -0.25, 0.6);
-			mesh.rotation.set(0, -Math.PI / 2, 0);
-			mesh.scale.set(0.5, 0.5, 0.5);
-
-			mesh.castShadow = true;
-			mesh.receiveShadow = true;
-
-			scene.add(mesh);
-
-			render();
-		});
 
 		// Lights
 		scene.add(new THREE.HemisphereLight(0x8d7c7c, 0x494966, 3));
 
 		const controls = new OrbitControls(camera, renderer.domElement);
-		controls.addEventListener('change', render);
 		controls.target.set(0, 0, 2);
 		controls.update();
 
-		function render() {
-			camera.lookAt(cameraTarget);
+		animate();
 
-			renderer.render(scene, camera);
+		function animate() {
+			requestAnimationFrame( animate );
+			renderer.render( scene, camera );
 		}
 
 		// Clean up the Three.js scene on component unmount
